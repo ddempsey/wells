2a17eb393e005c38d4219eb5e16eae4b2313785e
David Dempsey <david.dempsey@canterbury.ac.nz>
2021-09-01 19:03:48 +1200

    finish source protection notebook

--- travel_time.py
+++ travel_time.py
@@ -1,13 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import cm
-from scipy.linalg.basic import solve
 from scipy.special import lambertw as LW
 from scipy.optimize import root
 from scipy.integrate import solve_ivp
 
 from ipyleaflet import Map, basemaps, Marker, Polyline, Polygon, DivIcon, AwesomeIcon
-from ipywidgets import HBox, VBox, IntSlider, FloatSlider, Button, Layout
+from ipywidgets import HBox, VBox, IntSlider, FloatSlider, Button, Layout, BoundedFloatText, FloatLogSlider
 from pyproj import Proj, transform
 
 from functools import partial
@@ -18,15 +17,202 @@ from scipy.special import expi
 inProj = Proj('epsg:32759')
 outProj = Proj('epsg:4326')
 
-class A(object):
-    def __init__(self):
-        pass
+# class to handle widgets and updating map with contours
+class GroundwaterMap(Map):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.wells=[]
+        self.contour_labels=[]
+    def add_well(self, location, icon=None):
+        well = Marker(location=location, draggable=True, icon=icon)    
+        self.add_layer(well)
+        self.wells.append(well)
+    def update_contours(self, ps, ts):
+        for p,p0 in zip(ps,self.ps0):
+            p0.locations = p.locations
 
+        [self.remove_layer(tk) for tk in self.contour_labels]
+
+        self.contour_labels=[]
+        for ti in ts:
+            i = DivIcon(html=ti._text+'&nbsp;m')
+            tsm = Marker(location=[ti._x, ti._y], icon=i)
+            self.add_layer(tsm)
+            self.contour_labels.append(tsm)
+    def on_change(self,event):
+        self.func()
+    def configure(self, widgets, func):
+        self.widgets=widgets
+        self.func=func
+
+        # initial drawdown contours        
+        self.func()
+
+        # configure interactions and feedbacks
+        for w in list(self.widgets.values())+self.wells:
+            w.observe(self.on_change)
+        
+        def on_zoom_change(self, *args, **kwargs):
+            mi = args[0]['owner']
+            bnds=[[mi.south, mi.west],[mi.north, mi.east]]
+            func(bnds=bnds)
+
+        self.observe(partial(on_zoom_change, self), 'zoom')
+        
+        def handle_map_interaction(self, type, **kwargs):
+            if type!='mouseup': 
+                return
+            self.func()
+
+        self.on_interaction(partial(handle_map_interaction, self))
+    def travel_time(self, dhdx, n, b, xw=0, yw=0, bnds=None):
+        '''
+            t : float
+                time [days]
+            dhdx : float
+                hydraulic gradient [m/m]
+            Q : float
+                extraction rate [m^3/s]
+            n : float
+                porosity [-]
+            b : float
+                aquifer thickness [m]
+            theta : float
+                angle between x axis and dominant flow direction
+            xw : float
+                x location of well
+            yw : float
+                y location of well
+        '''
+        
+        T = 0.05
+        # get parameters
+        Q = self.widgets['Q'].value/1.e3
+        t = self.widgets['t'].value
+        theta = (270-self.widgets['th'].value)/180.*3.1416
+        dhdx = self.widgets['q'].value/1.e3
+        q = dhdx*T/b
+
+        # compute contour
+        if Q < 1.e-5:
+            # special case, zero pumping
+            xout=np.array([xw])
+            yout=np.array([yw])
+        else:
+            # compute dimensionless solution
+            t*=24*3600    # convert days to seconds
+            t0 = 2.*np.pi*q**2/(b*n*Q)*t
+            x0,y0 = travel_time_dimensionless(t0)
+            x = Q*x0/(2*np.pi*q)
+            y = Q*y0/(2*np.pi*q)
+            x,y = np.concatenate([x, x[::-1]]), np.concatenate([y, -y[::-1]])
+            x,y = (np.cos(theta)*x-np.sin(theta)*y, np.sin(theta)*x+np.cos(theta)*y)
+            xout=x+xw
+            yout=y+yw
+
+        # add or update travel time contour
+        lat,lon = xy2ll(xout,yout,*self.wells[0].location)
+        try:
+            self.tt_line.locations=list(zip(lat,lon))
+        except AttributeError:
+            self.tt_line = Polyline(locations=list(zip(lat,lon)), color='red', fill=False, weight=2)
+            self.add_layer(self.tt_line)
+
+        # add or update piezometric surface
+        if bnds is None:
+            bnds = self.bounds
+            if len(bnds) == 0:
+                bnds = ((-43.53118921794094, 172.62774467468262), 
+                    (-43.506293197337435, 172.70936965942383))
+        
+        ps, ts = self.TheisContours(T, [Q], bnds, [dhdx, theta], levels=np.arange(-10,20,1))
+
+        try:
+            self.update_contours(ps,ts)
+        except AttributeError:
+            self.ps0=ps
+            for polygons in self.ps0:
+                self.add_layer(polygons)
+    def superposition(self, T, bnds=None):
+        qs = [self.widgets['Q{:d}'.format(i)].value/1.e3 for i,w in enumerate(self.wells)]
+
+        # add or update piezometric surface
+        if bnds is None:
+            bnds = self.bounds
+            if len(bnds) == 0:
+                bnds = ((-43.54369559037467, 172.58697509765628), 
+                    (-43.493903600645126, 172.7502250671387))
+        ps, ts = self.TheisContours(T, qs, bnds)
+
+        try:
+            self.update_contours(ps,ts)
+        except AttributeError:
+            self.ps0=ps
+            for polygons in self.ps0:
+                self.add_layer(polygons)        
+    def TheisContours(self, T, qs, bnds=None, grad=[0,0], levels=(0.5,0.75,1,1.25,1.5,1.75,2.0)):
+        
+            
+        lats = []
+        lons = []
+        for w in self.wells:
+            lat,lon = w.location
+            lats.append(lat); lons.append(lon)
+        xs,ys = transform(outProj, inProj, lats, lons)
+        if bnds is None:
+            xs2,ys2 = xs,ys
+        else:
+            for bnd in bnds:
+                lat,lon = bnd
+                lats.append(lat); lons.append(lon)
+                xs2,ys2 = transform(outProj, inProj, lats, lons)
+        n = 100
+        x0,x1 = np.min(xs2), np.max(xs2)
+        xr = x1-x0
+        y0,y1 = np.min(ys2), np.max(ys2)
+        yr = y1-y0
+        xx,yy = np.meshgrid(np.linspace(x0-0.05*xr,x1+0.05*xr,n), 
+            np.linspace(y0-0.05*yr,y1+0.05*yr,n))
+        hh = 0.*xx+grad[0]*(np.cos(grad[1])*(xx-xs[0]) + np.sin(grad[1])*(yy-ys[0]))#/1.e3
+        try:
+            t = self.widgets['t'].value
+        except KeyError:
+            t = 100.
+        for w,x,y,q in zip(self.wells,xs,ys,qs):
+            hh += Theis(np.sqrt((xx.flatten()-x)**2+(yy.flatten()-y)**2), t*24*3600, T, 1.e-4, q).reshape(xx.shape)
+        lat,lon=transform(inProj,outProj,xx.flatten(),yy.flatten())
+        cs = plt.contourf(lat.reshape(xx.shape), lon.reshape(yy.shape), hh, 
+            levels=levels, extend='both')
+        ts = plt.clabel(cs, levels=[l for l,a in zip(cs.levels, cs.allsegs) if len(a)>0])
+        plt.close()
+        
+        allsegs = cs.allsegs
+        allkinds = cs.allkinds
+        cmap = cm.Blues
+        colors = ['#%02x%02x%02x' % tuple(int(j*255) for j in cmap(i)[:3]) for i in np.linspace(0,1,len(allsegs))]
+        alphas = np.linspace(0.2,0.7,len(allsegs))
+        ps = []
+        for clev in range(len(cs.allsegs)):
+            kinds = None if allkinds is None else allkinds[clev]
+            segs = split_contours(allsegs[clev], kinds)
+            polygons = Polygon(
+                            locations=[p.tolist() for p in segs],
+                            color='yellow',
+                            weight=1,
+                            opacity=1.,
+                            fill_color=colors[clev],
+                            fill_opacity=alphas[clev]
+            )
+            ps.append(polygons)
+        return ps,ts
+
+# drawdown functions
 def W(u): 
     return -expi(-u)
 def Theis(r,t,T,S,Q):
     return Q/(4*np.pi*T)*W(r**2*S/(4*T*t))
 
+# travel time functions
 def dydxf(x,y,t0): 
     '''
     sin(y) x/y+cos(y) = -sin(y) dy/dx + sin(y)/y - sin(y) x/y^2 dy/dx +cos(y)*x/y dy/dx
@@ -45,7 +231,6 @@ def dydxf(x,y,t0):
     a = np.cos(y)*x/y-np.sin(y)-np.sin(y)*x/y**2
     b = np.exp(x-t0)-np.sin(y)/y
     return np.array([np.min([(b/a)[0], 1.e20]),])
-
 def d2ydx2(x,y,t0):
     '''
     dy/dx = b/a
@@ -58,7 +243,6 @@ def d2ydx2(x,y,t0):
     dadx = np.cos(y)/y + x*(y*np.sin(y)+np.cos(y))/y**2*dydx-np.cos(y)*dydx-np.sin(y)/y**2-(y*np.cos(y)-2*np.sin(y))/y**3*dydx
     dbdx = np.exp(x-t0)-(y*np.cos(y)-np.sin(y))/y**2*dydx
     return np.array([np.min([((a*dbdx-b*dadx)/a**2)[0], 1.e20]),])
-
 def travel_time_dimensionless(t0):
     ''' implementing solution from Bear and Jacobs (1965)
     '''
@@ -74,90 +258,11 @@ def travel_time_dimensionless(t0):
     sol.y[0][-1]=0 
     return sol.t, sol.y[0]
 
-def travel_time(t, q, Q, n, b, theta=0., xw=0, yw=0):
-    '''
-        t : float
-            time [days]
-        q : float
-            specific discharge [m/s]
-        Q : float
-            extraction rate [m^3/s]
-        n : float
-            porosity [-]
-        b : float
-            aquifer thickness [m]
-        theta : float
-            angle between x axis and dominant flow direction
-        xw : float
-            x location of well
-        yw : float
-            y location of well
-    '''
-    t*=24*3600    # convert days to seconds
-    t0 = 2.*np.pi*q**2/(b*n*Q)*t
-    x0,y0 = travel_time_dimensionless(t0)
-    x = Q*x0/(2*np.pi*q)
-    y = Q*y0/(2*np.pi*q)
-    x,y = np.concatenate([x, x[::-1]]), np.concatenate([y, -y[::-1]])
-    x,y = (np.cos(theta)*x-np.sin(theta)*y, np.sin(theta)*x+np.cos(theta)*y)
-    return x+xw,y+yw
-
-def test():
-    f,ax = plt.subplots(1,1)
-    for t0 in np.logspace(-1,1,10):
-        x,y = travel_time(t0, 1., 2*np.pi, 1, 1, np.pi/4.*0.)
-        ax.plot(x,y,'k-')
-    x,y = travel_time(1.e7, 1.e-4, 0.1, 0.1, 10., theta=3.1416/4.)
-    ax.plot(x,y,'b-')
-    ax.axhline(0, color='k', linestyle=':', linewidth=0.5)
-    ax.axvline(0, color='k', linestyle=':', linewidth=0.5)
-    ax.set_aspect('equal')
-    plt.savefig('travel_time_test.png', dpi=400)
-
+# map functions
 def xy2ll(x,y,lat0,lon0):   
     x0,y0=transform(outProj, inProj, lat0, lon0)#, lat0)
     lat,lon = transform(inProj,outProj,x+x0,y+y0)
     return list(lat), list(lon)
-
-def TheisContours(T, ws, qs):
-    lats = []
-    lons = []
-    for w in ws:
-        lat,lon = w.location
-        lats.append(lat); lons.append(lon)
-    xs,ys = transform(outProj, inProj, lats, lons)
-    n = 200
-    xx,yy = np.meshgrid(np.linspace(np.min(xs)-1.e4,np.max(xs)+1.e4,n), 
-        np.linspace(np.min(ys)-1.e4,np.max(ys)+1.e4,n))
-    hh = 0.*xx
-    for w,x,y,q in zip(ws,xs,ys,qs):
-        hh += Theis(np.sqrt((xx.flatten()-x)**2+(yy.flatten()-y)**2), 100.*24*3600, T, 1.e-4, q).reshape(xx.shape)
-    lat,lon=transform(inProj,outProj,xx.flatten(),yy.flatten())
-    cs = plt.contourf(lat.reshape(xx.shape), lon.reshape(yy.shape), hh, 
-        levels=(0.5,0.75,1,1.25,1.5,1.75,2.0), extend='both')
-    ts = plt.clabel(cs)
-    plt.close()
-    
-    allsegs = cs.allsegs
-    allkinds = cs.allkinds
-    cmap = cm.Blues
-    colors = ['#%02x%02x%02x' % tuple(int(j*255) for j in cmap(i)[:3]) for i in np.linspace(0,1,len(allsegs))]
-    alphas = np.linspace(0.2,0.7,len(allsegs))
-    ps = []
-    for clev in range(len(cs.allsegs)):
-        kinds = None if allkinds is None else allkinds[clev]
-        segs = split_contours(allsegs[clev], kinds)
-        polygons = Polygon(
-                        locations=[p.tolist() for p in segs],
-                        color='yellow',
-                        weight=1,
-                        opacity=1.,
-                        fill_color=colors[clev],
-                        fill_opacity=alphas[clev]
-        )
-        ps.append(polygons)
-    return ps,ts
-
 def split_contours(segs, kinds=None):
     """takes a list of polygons and vertex kinds and separates disconnected vertices into separate lists.
     The input arrays can be derived from the allsegs and allkinds atributes of the result of a matplotlib
@@ -198,81 +303,50 @@ def split_contours(segs, kinds=None):
             new_segs.append(seg[boundaries[b]+(1 if b>0 else 0):boundaries[b+1]])
     return new_segs
 
+# widget functions
 def travel_time_fun():
-    x,y = travel_time(100., 1.e-4, 0.1, 0.1, 10., theta=3.1416/4.*3)
     center=[-43.51876443245584, 172.66858981519297]
-    zoom=14
-    m = Map(basemap=basemaps.Esri.WorldImagery, center=center, zoom=zoom)
-    well = Marker(location=center, draggable=True)    
-    m.add_layer(well)
-    lat,lon = xy2ll(x,y,*center)
-    line = Polyline(locations=list(zip(lat,lon)), color='red', fill=False, weight=2)
-    m.add_layer(line)
-    t = IntSlider(value=100, description=r'$t$ [day]', min=10, max = 365, step = 20, continuous_update = False)
-    Q = FloatSlider(value=100, description=r'$Q$ [L/s]', min = 20, max = 200, step = 20, continuous_update = False)
-    def on_change(event):
-        x,y = travel_time(t.value, 1.e-4, Q.value/1.e3, 0.1, 10., theta=3.1416/4.*3)
-        lat,lon = xy2ll(x,y,*well.location)
-        line.locations = list(zip(lat,lon))
-    Q.observe(on_change)#_Q, names='value')
-    well.observe(on_change)#, names='location')
-    t.observe(on_change)
-    return VBox([m, HBox([Q, t])])
-
+    m = GroundwaterMap(basemap=basemaps.Esri.WorldImagery, center=center, zoom=14)
+    t = IntSlider(value=100, description=r'$t_t$ [day]', min=10, max = 365, step = 20, 
+        continuous_update = False, layout=Layout(max_width='250px'))
+    Q = FloatSlider(value=100, description=r'pumping [L/s]', min = 0, max = 200, step = 20, 
+        continuous_update = False, layout=Layout(max_width='250px'))
+    q = FloatSlider(value=1.5, description=r'$dh/dx$ [m/km]', min = 0.5, max = 3, step = 0.5, 
+        continuous_update = False, layout=Layout(max_width='270px'))
+    th = BoundedFloatText(value=135, min=0, max=180, description='flow dir. [$^{\circ}$]',layout=Layout(max_width='150px'))
+    m.add_well(center)
+    m.configure(widgets={'t':t, 'Q':Q, 'q':q, 'th':th}, func=partial(m.travel_time, 1.e-4, 0.03, 10.))
+    return VBox([m, HBox([Q, t, q, th])])
 def superposition_fun(T=0.025):
-    tc = partial(TheisContours, T)
-    c = [-43.51876443245584, 172.66858981519297]
-    m = Map(basemap=basemaps.Esri.WorldImagery, center=c, zoom=13)
-    ws = []
+    center=[-43.51876443245584, 172.66858981519297]
+    m = GroundwaterMap(basemap=basemaps.Esri.WorldImagery, center=center, zoom=13)
     Qs = []
     bs = []
     for c in ['green','lightblue','red','pink']:
         icon = AwesomeIcon(name='fa-tint', marker_color=c, icon_color='black', spin=False)
-        w = Marker(location=[-43.51876443245584+(random()-0.5)*0.01, 
-            172.66858981519297+(random()-0.5)*0.01], draggable=True, icon=icon)    
-        m.add_layer(w)
-        ws.append(w)
-        
+        m.add_well([-43.51876443245584+(random()-0.5)*0.01, 
+            172.66858981519297+(random()-0.5)*0.01], icon=icon)
         Q = FloatSlider(value=10, description=r'$Q$ [L/s]', min = 0, max = 40, step = 5, 
             continuous_update = False, layout=Layout(max_width='230px'),
             style = {'description_width': '60px'})
         b = Button(disabled=False,icon='fa-tint',layout=Layout(max_width='230px'))
         b.style.button_color = c
         Qs.append(Q)
-        bs.append(b)        
+        bs.append(b)  
 
-    qs = [Q.value/1.e3 for Q in Qs]
-    ps0,ts = tc(ws,qs)
-    for polygons in ps0:
-        m.add_layer(polygons)
-    a = A()
-    a.tsms=[]
-    for t in ts:
-        i = DivIcon(html=t._text)
-        tsm = Marker(location=[t._x, t._y], icon=i)
-        m.add_layer(tsm)
-        a.tsms.append(tsm)
-
-    def on_change(a, event):
-        qs = [Q.value/1.e3 for Q in Qs]
-        ps, ts = tc(ws, qs)
-        for p,p0 in zip(ps,ps0):
-            p0.locations = p.locations
-
-        for t in a.tsms:m.remove_layer(t)
-
-        tsms=[]
-        for t in ts:
-            i = DivIcon(html=t._text)
-            tsm = Marker(location=[t._x, t._y], icon=i)
-            m.add_layer(tsm)
-            tsms.append(tsm)
-        a.tsms = tsms
-
-    for w in ws+Qs:
-        w.observe(partial(on_change,a))
-    # w2.observe(partial(on_change,a))
+    m.configure(widgets=dict([('Q{:d}'.format(i),Q) for i,Q in enumerate(Qs)]), 
+        func=partial(m.superposition, T))
     return VBox([m, HBox([VBox([b,Q]) for Q,b in zip(Qs,bs)])])
+def theis_fun():
+    center=[-43.51876443245584, 172.66858981519297]
+    m = GroundwaterMap(basemap=basemaps.Esri.WorldImagery, center=center, zoom=13)
+    icon = AwesomeIcon(name='fa-tint', marker_color='green', icon_color='black', spin=False)
+    m.add_well([-43.5187, 172.6685], icon=icon)
+    Q = FloatSlider(value=25, description=r'$Q$ [L/s]', min = 0, max = 100, step = 5, 
+        continuous_update = False, layout=Layout(max_width='230px'),
+        style = {'description_width': '60px'})
+    m.configure(widgets={'Q0':Q}, func=partial(m.superposition, 0.025))
+    return VBox([m, Q])
 
 if __name__ == "__main__":
-    superposition_fun()
+    travel_time_fun()
